00d3c84aa9d96673d56e9efe76444e3b9967c52a
ksk001100 <hm.pudding0715@gmail.com>
2020-02-18 04:13:36 +0900

    add documentation comments to context.rs

--- src/context.rs
+++ src/context.rs
@@ -1,11 +1,28 @@
 use crate::{Flag, FlagValue, FlagType};
 
+/// `Context` type
+/// This type is used only for `Action` arguments
 pub struct Context {
+    /// `Vec<String>` with flags and flag values ​​removed from command line arguments
     pub args: Vec<String>,
+    /// `Vec` that stores flag name and flag value as tuple
     flags: Option<Vec<(String, Option<FlagValue>)>>
 }
 
 impl Context {
+    /// Create new instance of `Context`
+    /// Parse processing using `Vec<String>` command line argument and `Vec<Flag>` as arguments
+    ///
+    /// Example
+    ///
+    /// ```
+    /// use std::env;
+    /// use seahorse::{Context, Flag, FlagType};
+    ///
+    /// let args: Vec<String> = env::args().collect();
+    /// let flag = Flag::new("bool", "cli cmd [arg] --bool", FlagType::Bool);
+    /// let context = Context::new(args, Some(vec![flag]));
+    /// ```
     pub fn new(args: Vec<String>, flags: Option<Vec<Flag>>) -> Self {
         let mut v = Vec::new();
         let mut parsed_args = args.clone();
@@ -29,12 +46,29 @@ impl Context {
         Self { args: parsed_args, flags: flags_val }
     }
 
+    /// Get flag value
     fn option_flag_value(&self, name: &str) -> Option<&FlagValue> {
         self.flags.as_ref()
             .and_then(|flags| flags.into_iter().find(|flag| flag.0 == name.to_string()))
             .and_then(|flag| flag.1.as_ref())
     }
 
+    /// Get bool flag
+    ///
+    /// Example
+    ///
+    /// ```
+    /// use std::env;
+    /// use seahorse::{Context, Flag, FlagType};
+    ///
+    /// let args: Vec<String> = env::args().collect();
+    /// let flag = Flag::new("bool", "cli cmd [arg] --bool", FlagType::Bool);
+    /// let context = Context::new(args, Some(vec![flag]));
+    ///
+    /// if context.bool_flag("bool") {
+    ///     println!("true");
+    /// }
+    /// ```
     pub fn bool_flag(&self, name: &str) -> bool {
         match self.option_flag_value(name) {
             Some(FlagValue::Bool(val)) => {
@@ -44,6 +78,23 @@ impl Context {
         }
     }
 
+    /// Get string flag
+    ///
+    /// Example
+    ///
+    /// ```
+    /// use std::env;
+    /// use seahorse::{Context, Flag, FlagType};
+    ///
+    /// let args: Vec<String> = env::args().collect();
+    /// let flag = Flag::new("string", "cli cmd [arg] --string [string]", FlagType::String);
+    /// let context = Context::new(args, Some(vec![flag]));
+    ///
+    /// match context.string_flag("string") {
+    ///     Some(s) => println!("{}", s),
+    ///     None => println!("Not found string...")
+    /// }
+    /// ```
     pub fn string_flag(&self, name: &str) -> Option<String> {
         match self.option_flag_value(name) {
             Some(FlagValue::String(val)) => Some((&val).to_string()),
@@ -51,6 +102,23 @@ impl Context {
         }
     }
 
+    /// Get int flag
+    ///
+    /// Example
+    ///
+    /// ```
+    /// use std::env;
+    /// use seahorse::{Context, Flag, FlagType};
+    ///
+    /// let args: Vec<String> = env::args().collect();
+    /// let flag = Flag::new("int", "cli cmd [arg] --int [int]", FlagType::Int);
+    /// let context = Context::new(args, Some(vec![flag]));
+    ///
+    /// match context.int_flag("int") {
+    ///     Some(i) => println!("{}", i),
+    ///     None => println!("Not found int number...")
+    /// }
+    /// ```
     pub fn int_flag(&self, name: &str) -> Option<isize> {
         match self.option_flag_value(name) {
             Some(FlagValue::Int(val)) => Some(val.clone()),
@@ -58,6 +126,23 @@ impl Context {
         }
     }
 
+    /// Get float flag
+    ///
+    /// Example
+    ///
+    /// ```
+    /// use std::env;
+    /// use seahorse::{Context, Flag, FlagType};
+    ///
+    /// let args: Vec<String> = env::args().collect();
+    /// let flag = Flag::new("float", "cli cmd [arg] --float [float]", FlagType::Float);
+    /// let context = Context::new(args, Some(vec![flag]));
+    ///
+    /// match context.float_flag("float") {
+    ///     Some(f) => println!("{}", f),
+    ///     None => println!("Not found float number...")
+    /// }
+    /// ```
     pub fn float_flag(&self, name: &str) -> Option<f64> {
         match self.option_flag_value(name) {
             Some(FlagValue::Float(val)) => Some(val.clone()),
