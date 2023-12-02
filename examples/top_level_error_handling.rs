1026e11cb07fd76aa046a070c17f513ae788c098
Tim Burks <timburks@google.com>
2023-12-02 10:55:12 -0800

    Use internal error type in top_level_error_handling.rs

--- examples/top_level_error_handling.rs
+++ examples/top_level_error_handling.rs
@@ -1,5 +1,6 @@
 use seahorse::{App, Context, Flag, FlagType};
 use std::env;
+use std::fmt;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,7 +11,7 @@ fn main() {
         .version(env!("CARGO_PKG_VERSION"))
         .action_with_result(|c: &Context| {
             if c.bool_flag("error") {
-                Err(Box::new(std::io::Error::other("test error")))
+                Err(Box::new(Error))
             } else {
                 Ok(())
             }
@@ -26,3 +27,19 @@ fn main() {
         Err(e) => println!("{}", e),
     };
 }
+
+#[derive(Debug, Clone)]
+struct Error;
+
+// Generation of an error is completely separate from how it is displayed.
+// There's no need to be concerned about cluttering complex logic with the display style.
+//
+// Note that we don't store any extra info about the errors. This means we can't state
+// which string failed to parse without modifying our types to carry that information.
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "test error")
+    }
+}
+
+impl std::error::Error for Error {}
