ec2c7c9381baed329fa5d5e2f156913eb5e49390
Keisuke Toyota <hm.pudding0715@gmail.com>
2023-12-02 18:36:09 +0900

    Release 2.2.0

---
+++ examples/top_level_error_handling.rs
@@ -0,0 +1,32 @@
+use seahorse::{ActionError, App, Context, Flag, FlagType};
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let app = App::new("cli")
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .description(env!("CARGO_PKG_DESCRIPTION"))
+        .usage("multiple_app [command] [arg]")
+        .version(env!("CARGO_PKG_VERSION"))
+        .action_with_result(|c: &Context| {
+            if c.bool_flag("error") {
+                Err(ActionError {
+                    message: "ERROR...".to_string(),
+                })
+            } else {
+                Ok(())
+            }
+        })
+        .flag(
+            Flag::new("error", FlagType::Bool)
+                .description("error flag")
+                .alias("e"),
+        );
+
+    match app.run_with_result(args) {
+        Ok(_) => println!("OK"),
+        Err(e) => match e {
+            ActionError { message } => println!("{}", message),
+        },
+    };
+}
