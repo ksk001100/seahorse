994a7c97a3aef924c76a1cf4c829a123ffb82e48
Jakob Hilmer <jakob@hilmer.dk>
2024-12-28 17:11:36 +0100

    Normalize args '-abcd=10' as '-a -b -c -d 10'
    This is also the normal behavior of CLI arg parsers - you can group
    mutilple short options with a single dash.

--- src/app.rs
+++ src/app.rs
@@ -339,9 +339,22 @@ impl App {
     /// Split arg with "=" to unify arg notations.
     /// --flag=value => ["--flag", "value"]
     /// --flag value => ["--flag", "value"]
+    /// -abe => ["-a", "-b", "-e"]
+    /// -abef=32 => ["-a", "-b", "-e", "-f", "32"]
     fn normalized_args(raw_args: Vec<String>) -> Vec<String> {
         raw_args.iter().fold(Vec::<String>::new(), |mut acc, cur| {
-            if cur.starts_with('-') && cur.contains('=') {
+            if cur.starts_with('-') && !cur.starts_with("--") {
+                if cur.contains('=') {
+                    let splitted_flag: Vec<String> =
+                        cur.splitn(2, '=').map(|s| s.to_owned()).collect();
+                    let short_named = splitted_flag[0].chars().skip(1).map(|c| format!("-{}", c));
+                    acc.append(&mut short_named.collect());
+                    acc.append(&mut splitted_flag[1..].to_vec());
+                } else {
+                    let short_named = cur.chars().skip(1).map(|c| format!("-{}", c));
+                    acc.append(&mut short_named.collect());
+                }
+            } else if cur.starts_with('-') && cur.contains('=') {
                 let mut splitted_flag: Vec<String> =
                     cur.splitn(2, '=').map(|s| s.to_owned()).collect();
                 acc.append(&mut splitted_flag);
--- src/command.rs
+++ src/command.rs
@@ -244,7 +244,18 @@ impl Command {
 
     fn normalized_args(raw_args: Vec<String>) -> Vec<String> {
         raw_args.iter().fold(Vec::<String>::new(), |mut acc, cur| {
-            if cur.starts_with('-') && cur.contains('=') {
+            if cur.starts_with('-') && !cur.starts_with("--") {
+                if cur.contains('=') {
+                    let splitted_flag: Vec<String> =
+                        cur.splitn(2, '=').map(|s| s.to_owned()).collect();
+                    let short_named = splitted_flag[0].chars().skip(1).map(|c| format!("-{}", c));
+                    acc.append(&mut short_named.collect());
+                    acc.append(&mut splitted_flag[1..].to_vec());
+                } else {
+                    let short_named = cur.chars().skip(1).map(|c| format!("-{}", c));
+                    acc.append(&mut short_named.collect());
+                }
+            } else if cur.starts_with('-') && cur.contains('=') {
                 let mut splitted_flag: Vec<String> =
                     cur.splitn(2, '=').map(|s| s.to_owned()).collect();
                 acc.append(&mut splitted_flag);
